e9ea8cf3dba795089f2b8f7f4d494bdd46f51b76
Nikita Magomedeminov <nikiniks2005@gmail.com>
2025-11-22 22:35:05 +0200

    add: comment editing logic

--- frontend/src/v1/js/learning/solution.js
+++ frontend/src/v1/js/learning/solution.js
@@ -73,70 +73,10 @@ const fn = {
       })
     })
   },
-
-  initCommentModal: function () {
-    modalFormWrapper.modal({
-      show: false,
-    });
-    // Show EpicEditor when modal shown
-    modalFormWrapper.on('shown.bs.modal', function (event) {
-      const textarea = $(event.target).find('textarea').get(0);
-      UberEditor.init(textarea);
-      modalFormWrapper.css('opacity', '1');
-    });
-    // Show modal
-    $('.__edit', comment).click(function (e) {
-      e.preventDefault();
-      const $this = $(this);
-      $.get(this.href, function (data) {
-        modalFormWrapper.css('opacity', '1');
-        $('.inner', modalFormWrapper).html(data);
-        modalFormWrapper.modal('toggle');
-      }).fail(function (data) {
-        if (data.status === 403) {
-          const msg = 'Access denied. Probably, the time to edit the comment has expired.';
-          createNotification(msg, 'error');
-          $this.remove();
-        }
-      });
-    });
-
-    modalFormWrapper.on('submit', 'form', fn.onSubmitCommentModalForm);
-  },
-
-  onSubmitCommentModalForm: function (event) {
-    event.preventDefault();
-    let form = event.target;
-    // TODO: validate empty comment here
-    $.ajax({
-      url: form.action,
-      type: 'POST',
-      data: $(form).serialize(),
-    })
-      .done(function (json) {
-        if (json.success === 1) {
-          modalFormWrapper.modal('hide');
-          let target = comment.filter(function () {
-            return $(this).data('id') == json.id;
-          });
-          const textElement = $('.ubertext', target);
-          textElement.html(json.html);
-          UberEditor.render(textElement.get(0));
-          createNotification('Comment changed');
-        } else {
-          createNotification('Failed to update the comment', 'error');
-        }
-      })
-      .fail(function () {
-        createNotification('Failed to update the comment', 'error');
-      });
-    return false;
-  },
 };
 
 export function launch() {
   fn.initCommentForm();
   fn.initSolutionForm();
   fn.initJbaTab();
-  fn.initCommentModal();
 }
